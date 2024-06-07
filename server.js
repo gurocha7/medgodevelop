a4c50626f8c09f974832c5acf70a647a4719ce6a
Gustavo Rocha <gustavorocha.eir@gmail.com>
2024-06-07 18:43:13 -0300

    new deploy

--- server.js
+++ server.js
@@ -6,7 +6,7 @@ const app = express();
 app.use(express.static(path.join(__dirname, 'build/web')));
 
 app.get('/*', function(req, res) {
-  res.sendFile(path.join(__dirname, 'build/web', 'index.html'));
+  res.sendFile(path.join(__dirname, 'build/web', 'index.php'));
 });
 
 const port = process.env.PORT || 8080;
